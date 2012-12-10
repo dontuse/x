f840bb2aa325655acc439c9a91d2634fc3f28aa9
Andrey Bayandin <dontusecascade@gmail.com>
2012-12-10 08:47:16 +0600

    init

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$to  = "cukune@gmail.com , cukuneuss@yandex.ru , dontusecascade@mail.ru" ;
+
+$subject = "Birthday Reminders for August";
+
+$message = file_get_contents('mail.html');
+
+$headers  = "Content-type: text/html; charset=utf-8 \r\n";
+$headers .= "From: Birthday Reminder <birthday@example.com>\r\n"; 
+$headers .= "Bcc: birthday-archive@example.com\r\n"; 
+
+if(mail($to, $subject, $message, $headers)) {
+    print "ok" ;
+} else {
+    print "error" ;
+};
+?>

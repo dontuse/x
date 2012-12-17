27d116a7652ee92080cc0d31818c5dcc387ab812
Andrey Bayandin <cukune@gmail.com>
2012-12-17 16:31:25 +0600

    x2

--- send.php
+++ send.php
@@ -3,7 +3,7 @@ $to  = "cukune@gmail.com , cukuneuss@yandex.ru , dontusecascade@mail.ru" ;
 
 $subject = "Birthday Reminders for August";
 
-$message = file_get_contents('mail.html');
+$message = file_get_contents('mail-separated.html');
 
 $headers  = "Content-type: text/html; charset=utf-8 \r\n";
 $headers .= "From: Birthday Reminder <birthday@example.com>\r\n"; 
